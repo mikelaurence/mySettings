6a7010be6550fbb7a35c218d6ad72e58779df18b
Mike Laurence <mikelaurence@LaurenceLive.(none)>
2009-08-19 16:25:18 -0500

    added basic slider cell

---
+++ Code/Classes/SliderCell.h
@@ -0,0 +1,16 @@
+//
+//  SliderCell.h
+//  Menu
+//
+//  Created by Mike Laurence on 8/19/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SettingsCell.h"
+
+@interface SliderCell : SettingsCell {
+
+}
+
+@end
